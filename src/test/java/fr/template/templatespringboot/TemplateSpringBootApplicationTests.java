6cce9468d6d47ce4d82948544fdf6fa01e2a0f8b
JulienADAMI <mkltr309@gmail.com>
2024-01-15 20:19:18 +0100

    init repository

---
+++ src/test/java/fr/template/templatespringboot/TemplateSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.template.templatespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
